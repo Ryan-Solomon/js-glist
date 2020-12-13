b5797b8414d21ae0b00494cc513ea546e165fe60
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-13 13:29:06 -0600

    add item

--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 
 const createItemElement = (text) => {
   const containerDiv = document.createElement('div');
+  containerDiv.classList.add('item-component');
   const title = document.createElement('h2');
   const buttonContainer = document.createElement('div');
-  const editButton = document.createElement('button');
+  buttonContainer.classList.add('buttons');
   const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
 
   title.textContent = text;
 
   containerDiv.append(title);
-  buttonContainer.append(editButton, deleteButton);
+  buttonContainer.append(deleteButton);
   containerDiv.append(buttonContainer);
 
   return containerDiv;
 };
+
+// Consts
+const addItemButton = document.querySelector('.input button');
+const groceryItemsContainer = document.querySelector('.grocery-items');
+
+// Event Listeners
+
+addItemButton.addEventListener('click', () => {
+  const text = document.querySelector('.input input').value;
+  const divToAdd = createItemElement(text);
+  groceryItemsContainer.append(divToAdd);
+});
